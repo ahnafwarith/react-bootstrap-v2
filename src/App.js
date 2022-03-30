aa01e8e03c342966e171ef3365ee14848cb200a2
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-30 10:23:42 +0600

    installed react bootstrap and tried it out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is React Bootstrap</h1>
+      <Button variant='danger'>Click</Button>
+      <Spinner style={{ position: 'relative', top: '50px', right: '45px' }} animation="border" variant="dark" />
+
     </div>
   );
 }
